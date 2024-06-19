ff82ba4e3e0a86791a25454f576a5ce6db8d43bf
Axay17 <akshayteja04@gmail.com>
2024-06-20 01:26:18 +0530

    CSS Styles & Components added

---
+++ src/components/categoryList/CategoryList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./categoryList.module.css";
+
+const CategoryList = () => {
+  return (
+    <div classname="{styles.container}">
+      CategoryList
+    </div>
+  )
+}
+
+export default CategoryList
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./footer.module.css";
+
+
+const Footer = () => {
+  return (
+    <div classname="{styles.container}">
+      Navbar
+    </div>
+  )
+}
+
+export default Footer
